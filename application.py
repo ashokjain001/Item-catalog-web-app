68d0f6bd91c867e2c6935782296cd27f0bbf9eee
ashok <ashokjain001@gmail.com>
2017-11-12 20:09:55 -0600

    update database

--- application.py
+++ application.py
@@ -264,24 +264,19 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data.decode('utf-8')
-    print(type(access_token), access_token)
 
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
-    print(type(app_id), app_id)  
+
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    print(type(app_secret), app_secret)
+
     url = ('https://graph.facebook.com/oauth/access_token?grant_type='
            'fb_exchange_token&client_id=%s&client_secret=%s'
            '&fb_exchange_token=%s' % (app_id, app_secret, access_token))
 
     h = httplib2.Http()
     result = h.request(url, 'GET')[1].decode('utf-8')
-    print(type(result),'(*&(*&(*^&(*^*^(^*^(^(^^*&^(^(^(^(*^(^&', result)
-
-
-    data = json.loads(result)
 
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.9/me"
@@ -294,7 +289,8 @@ def fbconnect():
         that it can be used directly in the graph api calls
     '''
     token = result.split(',')[0].split(':')[1].replace('"', '')
-    print('Token', token)
+    
+    # API call to retrieve user info data 
     url = ('https://graph.facebook.com/v2.9/me?access_token=%s'
            '&fields=name,id,email' % token)
     h = httplib2.Http()
@@ -310,8 +306,8 @@ def fbconnect():
     # The token must be stored in the login_session in order to properly logout
     login_session['access_token'] = token
 
-    # Get user picture
-    url = ('https://graph.facebook.com/v2.8/me/picture?access_token=%s'
+    # seperate API call to retrieve user picture
+    url = ('https://graph.facebook.com/v2.9/me/picture?access_token=%s'
            '&redirect=0&height=200&width=200' % token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
